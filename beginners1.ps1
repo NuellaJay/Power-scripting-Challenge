64a5b8e046f78dfde6a4236dc8a0e577334cbdc8
NuellaJay <jeclievesemmannuella@gmail.com>
2021-09-28 16:40:45 +0200

    Counting challenge

---
+++ beginners1.ps1
@@ -0,0 +1,16 @@
+$t= 0
+for ($i =2; $i -le 100; $i +=2) {
+    $t +=$i
+}
+$t
+ 
+
+1..100 | Where-Object {-Not($_%2)} | Measure-Object -sum
+ 
+$t = 0
+foreach ($n in (1..100)) {
+    if ($n/2 -is [int]) {
+        $t += $n
+    }
+}
+$t 
